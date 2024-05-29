1b3104454bb0f3df9851c3b26682190815e38bfc
Binil B <binil.b.re@gmail.com>
2024-05-29 19:34:40 +0530

    package

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
  
 export default defineConfig({
-  schema: "./schema/*",
+  schema: ".configs/schema.js/*",
   out: "./drizzle",
   dialect: 'postgresql',
   dbCredentials: {
