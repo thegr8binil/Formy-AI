6f4477a19aa219868c1be6388856234cc33fe40b
Binil B <binil.b.re@gmail.com>
2024-05-31 15:48:10 +0530

    live view

--- app/liveView/[formId]/page.js
+++ app/liveView/[formId]/page.js
@@ -1,7 +1,36 @@
-export default function LiveView(){
-    return (
-        <div>
-            <h1>Live View</h1>
-        </div>
-    )
+"use client";
+
+import { db } from "@/configs";
+import { eq } from "drizzle-orm";
+import { useEffect, useState } from "react";
+import { forms } from "@/configs/schema";
+import FormFields from "@/components/formFields";
+
+export default function LiveView({ params }) {
+  const [record, setRecord] = useState();
+  const [jsonData, setJsonData] = useState([]);
+  useEffect(() => {
+    params && getFormData();
+  }, [params]);
+
+  const getFormData = async () => {
+    const data = await db
+      .select()
+      .from(forms)
+      .where(eq(forms.id, Number(params?.formId)));
+    setRecord(data[0]);
+    const jsonDataString = data[0].jsonForm.replace(/^```json\n|```$/g, "");
+    const jsonData = JSON.parse(jsonDataString);
+    setJsonData(jsonData);
+  };
+
+  return (
+    <div>
+      <FormFields
+        jsonData={jsonData}
+        deleteFiled={console.log()}
+        onFeildUpdate={console.log()}
+      />
+    </div>
+  );
 }
